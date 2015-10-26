b4400f0c1c3602806862509856c9da138a41bcad
Joel Shuman <joel.shumansg@gmail.com>
2015-10-25 18:56:26 -0700

    Fizzbuzz implemented

--- Chapter_1_Python_Syntax/code/3_fizzbuzz.py
+++ Chapter_1_Python_Syntax/code/3_fizzbuzz.py
@@ -1,15 +1,23 @@
 import sys
 
-def fizzbuzz(num, fizz, buzz):
+def fizzbuzz(num, fizz=3, buzz=5):
     '''
     INPUT: int, int, int
     OUTPUT: string
-
     Return "Fizz" if num is divisible by fizz,
            "Buzz" if num is divisible by buzz,
            "FizzBuzz" if num is dibisible by both fizz and buzz, and
            "" otherwise
     '''
+
+    if num % fizz == 0 and num % buzz ==0:
+      return "FizzBuzz"
+    elif num % fizz == 0:
+      return "Fizz"
+    elif num % buzz == 0:
+      return "Buzz"
+    else: return ""
+
     pass
 
 
